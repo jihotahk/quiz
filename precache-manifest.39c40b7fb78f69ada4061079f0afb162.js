358f941913900f1a8dac521ea89e92ee4319dac7
Jiho Tahk <jihotahk@gmail.com>
2019-04-07 17:33:10 -0500

    Updates

---
+++ precache-manifest.39c40b7fb78f69ada4061079f0afb162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  },
+  {
+    "revision": "5ac7b4c9d72f9d518a28",
+    "url": "/quiz/static/js/main.b7ae4db9.chunk.js"
+  },
+  {
+    "revision": "c05ff51464433a917c5e",
+    "url": "/quiz/static/js/2.8c449e24.chunk.js"
+  },
+  {
+    "revision": "5ac7b4c9d72f9d518a28",
+    "url": "/quiz/static/css/main.a48753ac.chunk.css"
+  },
+  {
+    "revision": "ff96d29f989b2f0c15f434a0a6586af3",
+    "url": "/quiz/index.html"
+  }
+];
